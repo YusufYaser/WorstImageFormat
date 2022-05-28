e2ef2b0b3f41cd0f62c2dfa15b4b7a5fa8ae0c48
YusufYaser <yusuf.kelany@gmail.com>
2022-05-28 22:47:42 +0200

    add scaling

--- main.py
+++ main.py
@@ -10,10 +10,13 @@
 import sys
 from tkinter import *
 from PIL import Image, UnidentifiedImageError
+import random
+import re
 
 if len(sys.argv) < 2:
-    print("Usage: " + sys.argv[0] + " <image_path>")
+    print("Usage: " + sys.argv[0] + " <image_path> [scale]")
     print("       " + sys.argv[0] + " convert <source> <output>")
+    print("       " + sys.argv[0] + " random <width> <height> <output>")
     exit(1)
 
 if sys.argv[1] == "convert":
@@ -50,13 +53,33 @@ if sys.argv[1] == "convert":
             data += chr(pixel[1])
             data += chr(pixel[2])
 
-    # how image file works
-    # ---
-    # version,width,height
-    # data
-    # ---
     output.write("1," + str(width) + "," + str(height) + "\n" + data)
     
+    sys.exit(0)
+
+if sys.argv[1] == "random":
+    if not len(sys.argv) == 5:
+        print("Usage: " + sys.argv[0] + " random <width> <height> <output>")
+        exit(1)
+
+    print("Creating image...")
+    
+    width = int(sys.argv[2])
+    height = int(sys.argv[3])
+
+    output = open(sys.argv[4], "w")
+
+    data = ""
+
+    for x in range(width):
+        for y in range(height):
+            r = int(random.randint(0, 255))
+            g = int(random.randint(0, 255))
+            b = int(random.randint(0, 255))
+            data += chr(r) + chr(g) + chr(b)
+    
+    output.write("1," + str(width) + "," + str(height) + "\n" + data)
+
     sys.exit(0)
 
 try:
@@ -66,8 +89,16 @@ except FileNotFoundError:
     exit(2)
 
 try:
+    try:
+        scale = int(sys.argv[2])
+    except ValueError:
+        scale = 1
+    except IndexError:
+        scale = 1
+
+
     print("Reading image")
-    print("0%\r", end="")
+    print(" 0%\r", end="")
 
     dim = image.readline().replace("\n", "").split(",")
     dim[0] = int(dim[2])
@@ -76,18 +107,15 @@ try:
     window = Tk()
     window.resizable(False, False)
     window.title(image.name)
-    window.geometry(str(dim[1]) + "x" + str(dim[0]))
+    window.geometry(str(int(dim[1] * scale)) + "x" + str(int(dim[0] * scale)))
 
     data = image.read()
-    pixels = []
-    while data:
-        pixels.append(data[:3])
-        data = data[3:]
+    pixels = re.findall(".{3}", data)
 
     canvas = Canvas(
         window,
-        width = dim[1],
-        height = dim[0],
+        width = int(dim[1] * scale),
+        height = int(dim[0] * scale),
         bg="#000"
     )
 
@@ -100,18 +128,19 @@ try:
     for pixel in pixels:
         rgb = (ord(pixel[0]), ord(pixel[1]), ord(pixel[2]))
         hex = '#%02x%02x%02x' % rgb
-        print(int(at / max * 100).__str__() + "%    \r", end="")
+        print(" " + int(at / max * 100).__str__() + "%    \r", end="")
 
         if x > dim[0]:
             x = 1
             y += 1
 
-
-        canvas.create_rectangle(
-            y, x, y, x,
-            outline="",
-            fill=hex
-        )
+        for sx in range(scale):
+            for sy in range(scale):
+                canvas.create_rectangle(
+                    y * scale + sx, x * scale + sy, y * scale + sx, x * scale + sy,
+                    outline = hex,
+                    fill = hex
+                )
         
         x += 1
         at += 1
@@ -122,7 +151,9 @@ try:
 
     window.mainloop()
 
-except IndexError or ValueError:
+except IndexError:
+    window.mainloop()
+except ValueError:
     window.mainloop()
 except KeyboardInterrupt:
     print("\nCancelled")
